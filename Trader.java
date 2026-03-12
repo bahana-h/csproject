8ae3843039454963492727402907c35bbf24e1bb
ovoloshyn997-me <ovoloshyn997@student.fuhsd.org>
2026-03-12 09:33:29 -0700

    Udpated trader.java
    methods are not all done though

--- Trader.java
+++ Trader.java
@@ -11,9 +11,137 @@ public class Trader implements Comparable<Trader>
     private TraderView myView;
     private Queue<String> mailbox;
 
-    // TODO complete class
+    //     Stores:
+    // Trader name
+    // Reference to Brokerage
+    // Mailbox (Queue<String>)
+
+    // Responsibilities:
+    // Request quotes
+    // Place orders
+    // Receive messages
+    // Display stored messages when logging in
+
+    // constructor
+    // sets all the fields
+    public Trader(Brokerage b, String n, String p)
+    {
+        brokerage = b;
+        screenName = n;
+        password = p;
+    }
+
+    // methods
+
+    //Compares this trader to another 
+    // by comparing their screen names case blind.
+    public int compareTo(Trader other)
+    {
+        if (screenName == other.screenName)
+        {
+            return 1;
+        }
+
+        else
+        {
+            return 0;
+        }
 
 
+    }
+
+    //Indicates whether some other trader is "equal to" this one, 
+    // based on comparing their screen names case blind. 
+    // This method will throw a ClassCastException 
+    // if other is not an instance of Trader.
+    public boolean equals(Object other)
+    {
+        // check if other is not a trader
+        return true;
+        
+
+    }
+
+    public String getName()
+    {
+        return screenName;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void getQuote(String symbol)
+    {
+        // call brokerage's get quote
+        return;
+    }
+
+
+
+
+    public boolean hasMessages()
+    {
+        //return WHATEVER MESSAGE CONTAINER.isEmpty();
+        return true;
+    }
+
+    // Sets a new TraderView for this trader and saves a reference to it in myView. 
+    // Removes and displays all the messages, if any, from this trader's mailbox 
+    // by calling myView.showMessage(msg) for each message.
+    public void setView(TraderView view)
+    {
+
+    }
+
+
+    // Places a given order with the brokerage by calling brokerage's placeOrder.
+    public void placeOrder(TradeOrder other)
+    {
+
+    }
+
+
+    // Logs out this trader. Calls brokerage's logout for this trader. 
+    // Sets myView to null (this method is called from a 
+    // TraderWindow's window listener when the "close window" button is clicked)
+    public void quit()
+    {
+
+    }
+
+
+    // Adds msg to this trader's mailbox and displays all messages. 
+    // If this trader is logged in (myView is not null) 
+    // removes and shows all the messages in the mailbox 
+    // by calling myView.showMessage(msg) for each msg in the mailbox.
+    public void receiveMessage(String msg)
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     //
     // The following are for test purposes only
     //
