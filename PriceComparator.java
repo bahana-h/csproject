36b8454f9d7781eef9b219f1f766df398b729296
bahana-h <122423688+bahana-h@users.noreply.github.com>
2026-03-12 09:17:11 -0700

    pricecomparator constructor updates

--- PriceComparator.java
+++ PriceComparator.java
@@ -3,7 +3,40 @@
  */
 public class PriceComparator implements java.util.Comparator<TradeOrder>
 {
+    // i think its ascending flag
+    private boolean ascendingFlag;
+    /* Compares two TradeOrder objects by price. */
+    /* public PriceComparator()
+        Constructs a price comparator that compares two orders in ascending order. Sets the private boolean ascending flag to true. */
+    public PriceComparator() {
+        ascendingFlag = true;
+    }
 
-    // TODO complete class
+    /* public PriceComparator(boolean asc)
+        Constructs a price comparator that compares two orders in ascending or descending order. The order of comparison depends on the value of a given parameter. Sets the private boolean ascending flag to asc.
+
+        Parameters:
+        asc - if true, make an ascending comparator; otherwise make a descending comparator. */
+    public PriceComparator(boolean asc){
+        ascendingFlag = asc;
+    }
+
+    /* public int compare(TradeOrder order1,
+                   TradeOrder order2)
+        Compares two trade orders.
+
+        Specified by:
+        compare in interface java.util.Comparator<TradeOrder>
+        Parameters:
+        order1 - the first order
+        order2 - the second order
+        Returns:
+        0: if both orders are market orders.
+        -1: if order1 is market and order2 is limit.
+        1: if order1 is limit and order2 is market.
+        The difference in prices in cents: if both order1 and order2 are limit orders. In this last case, the difference returned is cents1 - cents2 or cents2 - cents1, depending on whether this is an ascending or descending comparator (ascending is true or false). You will need to round your answer otherwise floating point conversion to integer can cause errors. Be careful when rounding positive and negative values... */
+    public int compare(TradeOrder order1, TradeOrder order2) {
+        return 0;
+    }
 
 }
