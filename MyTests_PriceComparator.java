74004875d78525488e3a80f88a64418ca530cc7d
bahana-h <122423688+bahana-h@users.noreply.github.com>
2026-03-23 19:43:39 -0700

    name put in

--- MyTests_PriceComparator.java
+++ MyTests_PriceComparator.java
@@ -1,6 +1,6 @@
 public class MyTests_PriceComparator {
 
-    public static void test() {
+    public static void test() { // hannah
 
         System.out.println("\n===== PriceComparator Tests =====");
         // TODO - add tests for PriceComparator class
--- MyTests_StockExchange.java
+++ MyTests_StockExchange.java
@@ -1,6 +1,6 @@
 public class MyTests_StockExchange {
 
-    public static void test() {
+    public static void test() { // hannah
 
         System.out.println("\n===== StockExchange Tests =====");
         // TODO - add tests for StockExchange class
