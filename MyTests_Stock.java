fd56100d70bfd13d2b830e1615074aec7ae28beb
bahana-h <122423688+bahana-h@users.noreply.github.com>
2026-03-25 22:50:07 -0700

    one stock test

--- MyTests_Stock.java
+++ MyTests_Stock.java
@@ -1,10 +1,30 @@
 public class MyTests_Stock {
 
+    // hannah - so far idk abt the rest
+
+    // template form trade order again
+    // Shared test data 
+    private static String symbol = "GGGL";
+    private static boolean buyOrder = true;
+    private static boolean marketOrder = true;
+    private static int numShares = 123;
+    private static int numToSubtract = 24;
+    private static double price = 123.45;
+
+    private static Stock to = new Stock(symbol, "um a name", price);
+    private static Trader b = new Trader(null, "buyer", null);
+    private static Trader s = new Trader(null, "seller", null);
+
+
     public static void test() {
 
         System.out.println("\n===== Stock Tests =====");
-        // TODO - add tests for Stock class
+        testStockConstructor();
+    }
 
-
+    private static void testStockConstructor() {
+        System.out.println("\nRunning testStockConstructor...");
+        System.out.println("Expected: GGGL, 123.45");
+        System.out.println("Actual: " + to.getStockSymbol() + ", " + to.getLastPrice());
     }
 }
